293de5040e56e95f8dd2662d4dc5fcf4fc74a516
AFathi <me@ahmedbekhit.com>
2017-11-06 01:17:11 -0500

    Carthage update

---
+++ Example/NotchToolkit/NotchToolkit.h
@@ -0,0 +1,19 @@
+//
+//  NotchToolkit.h
+//  NotchToolkit
+//
+//  Created by Ahmed Bekhit on 11/6/17.
+//  Copyright © 2017 Ahmed Fathi Bekhit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotchToolkit.
+FOUNDATION_EXPORT double NotchToolkitVersionNumber;
+
+//! Project version string for NotchToolkit.
+FOUNDATION_EXPORT const unsigned char NotchToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotchToolkit/PublicHeader.h>
+
+
